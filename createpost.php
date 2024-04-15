e3982d65c3264b97e5c9244b91c282a791e84da4
jamesmb8 <james.briggs1109@gmail.com>
2024-04-15 11:48:20 +0100

    yes

---
+++ createpost.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Create Post</title>
+    <link rel="stylesheet" href="style.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+
+<body>
+    <div class="container">
+        <div class="wrapper">
+            <section class="post">
+                <header>Create Post</header>
+                <form action="#">
+                    <div class="content">
+                        <img src="User-image.png" alt="logo">
+                        <div class="details">
+
+                            <div class="privacy">
+                                <i class="fas fa-user-friends"></i>
+                                <span>Friends</span>
+                                <i class="fas fa-caret-down"></i>
+                            </div>
+                        </div>
+                    </div>
+                    <textarea placeholder="What's on your mind?" spellcheck="false" required></textarea>
+
+                
+                        <a href="mainpage.php" button class="post-button">Post </button>
+
+
+                        
+                </form>
+            </section>
+
+
+            
+
+</body>
+
+</html>
